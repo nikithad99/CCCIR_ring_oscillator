ea557b6f9a80c176cc0c96c66a31514814a96b7b
nikithad99 <nikithad1999@gmail.com>
2026-03-17 19:58:08 +0530

    Add ring and driver modules with output assignments

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_mattvenn_analog_ring_osc (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
@@ -19,5 +19,65 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+    wire ring_0_out;
+    wire ring_2_out;
+
+    ring ring_0(
+        .VDD(VDPWR),
+        .VSS(VGND),
+        .enable(ui_in[0]),
+        .out(ring_0_out)
+    );
+
+    driver driver_0(
+        .VDD(VDPWR),
+        .VSS(VGND),
+        .in(ring_0_out),
+        .out(ua[0])
+    );
+    
+    ring_2 ring_2(
+        .VDD(VDPWR),
+        .VSS(VGND),
+        .enable(ui_in[1]),
+        .out(ring_2_out)
+    );
+
+    driver driver_2(
+        .VDD(VDPWR),
+        .VSS(VGND),
+        .in(ring_2_out),
+        .out(ua[1])
+    );
+
+    // outputs 
+    assign uo_out[0] = ua[0];
+    assign uo_out[1] = ua[1];
+    assign uo_out[2] = VGND;
+    assign uo_out[3] = VGND;
+    assign uo_out[4] = VGND;
+    assign uo_out[5] = VGND;
+    assign uo_out[6] = VGND;
+    assign uo_out[7] = VGND;
+
+    // ties for the output enables
+    assign uio_out[0] = VGND;
+    assign uio_out[1] = VGND;
+    assign uio_out[2] = VGND;
+    assign uio_out[3] = VGND;
+    assign uio_out[4] = VGND;
+    assign uio_out[5] = VGND;
+    assign uio_out[6] = VGND;
+    assign uio_out[7] = VGND;
+
+    assign uio_oe[0] = VGND;
+    assign uio_oe[1] = VGND;
+    assign uio_oe[2] = VGND;
+    assign uio_oe[3] = VGND;
+    assign uio_oe[4] = VGND;
+    assign uio_oe[5] = VGND;
+    assign uio_oe[6] = VGND;
+    assign uio_oe[7] = VGND;
 
 endmodule
